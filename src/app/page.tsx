7d48063076cdb561ec6fdf8d48a543e63e4d7837
lokmane <lokmane.ghedied@gmail.com>
2023-11-26 18:48:01 +0000

    version 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-  const version = process.env.APP_VERSION
+  const version = process.env.APP_VERSION || '1'
   return (
     <main className="flex h-screen items-center justify-center text-4xl">
       KAMAL TEST - version {version}
