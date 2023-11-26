b3dd38430ee78d4c6b29718a14ba1b9acb0c3ebc
lokmane <lokmane.ghedied@gmail.com>
2023-11-26 17:01:13 +0000

    add app version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
+  const version = process.env.APP_VERSION
   return (
     <main className="flex h-screen items-center justify-center text-4xl">
-      KAMAL TEST
+      KAMAL TEST - version {version}
     </main>
   )
 }
